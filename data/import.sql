24a28605bd484e2ead26c1128af95692d4d2c058
Frankie <frankielkl33@hotmail.com>
2021-07-20 15:41:57 +0200

    Reglage heroku

---
+++ data/import.sql
@@ -0,0 +1,7 @@
+BEGIN;
+
+INSERT INTO "user"(last_name,first_name,mail,"location","password",profile_picture) VALUES(
+    "Rue", "33", "rue@oclock.io", " 48.866669, 2.33333","motdepasse tres secret","https://fr.web.img6.acsta.net/r_1280_720/newsv7/20/12/07/17/58/1442788.jpg"
+    )
+
+COMMIT;
