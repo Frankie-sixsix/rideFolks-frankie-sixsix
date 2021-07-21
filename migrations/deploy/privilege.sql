57544dce8e49d398e8830bb4407b247f4b909003
Frankie <frankielkl33@hotmail.com>
2021-07-21 10:04:37 +0200

    remove gitignore files

--- migrations/deploy/privilege.sql
+++ migrations/deploy/privilege.sql
@@ -3,7 +3,8 @@
 BEGIN;
 
 -- XXX Add DDLs here.
--- CREATE USER frankie with password 'rr4h33';
--- GRANT ALL PRIVILEGES ON DATABASE d59vot48lcn6fa TO frankie;
+
+
+git rm -r --cached . git add . git commit -m "remove gitignore files" git push
 
 COMMIT;
--- migrations/revert/privilege.sql
+++ migrations/revert/privilege.sql
@@ -2,8 +2,5 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
--- REVOKE ALL PRIVILEGES ON DATABASE "d59vot48lcn6fa" TO frankie;
--- DROP USER 'frankie';
 
 COMMIT;
