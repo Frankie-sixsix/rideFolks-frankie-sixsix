0aa6d7eface652c542409f881f1382a008a9bd7d
Frankie <frankielkl33@hotmail.com>
2021-07-22 13:51:45 +0200

    Ajout de reglage cors heroku

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 const router = require('./App/router');
-const cors = require('cors');
 
 const io = require("socket.io");
 
 const app = express();
 
 app.use(cors());
+
+app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
 
 app.use(express.json());
 
