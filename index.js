64035b8885ba7d1a10e517fef2fe3a56283a524a
Frankie <frankielkl33@hotmail.com>
2021-07-22 12:09:48 +0200

    Supression cors test pour herooku

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-
-
+const port = process.env.PORT || 3000;
 const router = require('./App/router');
+const cors = require('cors');
 
 const io = require("socket.io");
 
-
 const app = express();
 
-app.use(cors());
-
-const port = process.env.PORT || 3000;
+// app.use(cors());
 
 app.use(express.json());
-
 
 app.use(router);
 
