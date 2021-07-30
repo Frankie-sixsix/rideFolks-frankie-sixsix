6fe18e13fcbf86d6109943a7d47f2d43e8b69f83
Frankie <frankielkl33@hotmail.com>
2021-07-30 14:40:09 +0200

    [fix] socket.io - fixing request problem

--- App/Controller/socketController.js
+++ App/Controller/socketController.js
@@ -2,15 +2,15 @@
 const socketController = {
 
     test:(req,res)=>{
-        console.log("test");
-        const {id} = req.decoded;
-        const {content} = req.body;
-        const {idConv} = req.params;
-        console.log('Content:', content);
-        console.log('iDconv:', idConv);
-        console.log('idUser:', id);
+        // console.log("test");
+        // const {id} = req.decoded;
+        // const {content} = req.body;
+        // const {idConv} = req.params;
+        // console.log('Content:', content);
+        // console.log('iDconv:', idConv);
+        // console.log('idUser:', id);
 
-        res.sendFile('/var/www/html/simbad/apotheose/project-back/project-ridefolks-back/App/static/index.html');
+        // res.sendFile('/var/www/html/simbad/apotheose/project-back/project-ridefolks-back/App/static/index.html');
     }
 
         
