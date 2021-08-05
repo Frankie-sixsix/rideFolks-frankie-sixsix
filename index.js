b6e43dadc82fabdca95104f83880dbb1054637fc
Frankie <frankielkl33@hotmail.com>
2021-08-05 11:36:59 +0200

    Socket.io new test04

--- index.js
+++ index.js
@@ -89,9 +89,9 @@ io.on('connection', async (ws) => {
       // console.log('Array//', Array.from(io.sockets.adapter.rooms));
 
       console.log('RoomZ=', roomZ);
-      // io.emit('send_message_from_API', message);
+      io.emit('send_message_from_API', message);
 
-      io.sockets.in(roomZ).emit('send_message_from_API', message);
+      // io.sockets.in(roomZ).emit('send_message_from_API', message);
 
       console.log('Message = ', message);
 
