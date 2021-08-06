34e0b127a4b3371844d45d3ad71030f73f7ccb1b
Frankie <frankielkl33@hotmail.com>
2021-08-06 14:45:35 +0200

    S0CKET good version

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const server = app.listen(port, () => {
 
 
 
-
 // Reglage socket.io
 
 
@@ -35,89 +34,178 @@ const Message = require('./App/models/Message');
 
 
 
+let connectedUser = {};
+let idConv;
 
-let roomZ;
-
-io.on('connection', async (ws) => {
+io.on('connection', async(ws) => {
   console.log('>> socket.io - connected');
+  // console.log('>> ws', ws);
 
-  ws.on('create', function (room) {
-    ws.join(room.toString());
-    // io.in(room.toString()).emit('userJoin', 'yu');
+  ws.on('register', function(userId){
+    ws.userId = userId;
+    connectedUser[userId] = ws;
+    console.log('/_/_/connecteduser:',connectedUser);
+  });
 
-//   ws.on('create', function (room, callback) {
-//     ws.join(room.toString());
-//     callback('userJoin' + room);
-// });
-
-    
-    // const roster = io.sockets.adapter.rooms.get(room);
-
+  ws.on('create', function(room){
+    ws.join(room);
+    console.log('//ROOM:', room);
   });
 
 
-  ws.on('send_message_from_client', async (message) => {
+
+
+  ws.on('send_message_from_client', async(message) => {
     console.log("J'ai recu le message : send_message_from_client", message);
+      
 
+    
+    
     try {
-
+      
       // Verifie si la conversation existe 
       const existConv = await Conversation.checkIfConvExist(message.id_conv);
-      if (existConv) {
+      if(existConv){
         // On verifie si l'utilisateur a bien accée a cette conversation
-        const verifIfUserIsInConv = await Conversation.verifConv(message.sender_id, message.id_conv);
-        if (!verifIfUserIsInConv) {
-          return ("You are not part of this conversation");
-        }
-        // Si oui alors on enregistre le message dans la conv
-        const mess = new Message(message);
-        await mess.save(message.sender_id, message.id_conv);
+        const verifIfUserIsInConv = await Conversation.verifConv(message.sender_id,message.id_conv);
+              if(!verifIfUserIsInConv){
+                return ("You are not part of this conversation");
+              }
+              // Si oui alors on enregistre le message dans la conv
+              // const mess = new Message(message);    
+              // await mess.save(message.sender_id,message.id_conv);
+              
+              
+              
+            } else {
+              // Si non alors on crée une conversation PUIS on enregistre le message dans cette conversation
+              
+              idConv = await Conversation.createConv(message.sender_id,message.participant,message.name);
+              
+              // const verifIfUserIsInConv = await Conversation.verifConv(message.sender_id,message.id_conv);
+              // if(!verifIfUserIsInConv){
+                //     return ("You are not part of this conversation");
+                // }
+                
+              }
+              
+              console.log("IdConv", idConv);
+              // message.id_conv = idConv;
+              const mess = new Message(message);    
+              await mess.save(message.sender_id,message.id_conv);
 
-      } else {
-        // Si non alors on crée une conversation PUIS on enregistre le message dans cette conversation
 
-        idConv = await Conversation.createConv(message.sender_id, message.participant, message.name);
-        message.id_conv = idConv;
-        const mess = new Message(message);
-        await mess.save(message.sender_id, message.id_conv);
-      }
+              // console.log('Array//', Array.from(io.sockets.adapter.rooms));
+              io.emit('send_message_from_API', message );
+              
+              
+              
+            } catch (error) {
+              console.log(error);
+            }
+            
+              
+            });
+          });
+          
+          
+  
 
-      roomZ = message.id_conv;
 
-      // io.to(roomZ.toString()).emit('send_message_from_API', message);
+// Previous version socket.io
+// // Reglage socket.io
 
-      io.emit('send_message_from_API', message);
 
-      // io.in(message.id_conv).emit('send_message_from_API', message);
+// const io = new Server(server);
+
+// const Conversation = require('./App/models/Conversation');
+// const Message = require('./App/models/Message');
+
+
+
+
+// let roomZ;
+
+// io.on('connection', async (ws) => {
+//   console.log('>> socket.io - connected');
+
+//   ws.on('create', function (room) {
+//     ws.join(room.toString());
+//     // io.in(room.toString()).emit('userJoin', 'yu');
+
+// //   ws.on('create', function (room, callback) {
+// //     ws.join(room.toString());
+// //     callback('userJoin' + room);
+// // });
+
+    
+//     // const roster = io.sockets.adapter.rooms.get(room);
+
+//   });
+
+
+//   ws.on('send_message_from_client', async (message) => {
+//     console.log("J'ai recu le message : send_message_from_client", message);
+
+//     try {
+
+//       // Verifie si la conversation existe 
+//       const existConv = await Conversation.checkIfConvExist(message.id_conv);
+//       if (existConv) {
+//         // On verifie si l'utilisateur a bien accée a cette conversation
+//         const verifIfUserIsInConv = await Conversation.verifConv(message.sender_id, message.id_conv);
+//         if (!verifIfUserIsInConv) {
+//           return ("You are not part of this conversation");
+//         }
+//         // Si oui alors on enregistre le message dans la conv
+//         const mess = new Message(message);
+//         await mess.save(message.sender_id, message.id_conv);
+
+//       } else {
+//         // Si non alors on crée une conversation PUIS on enregistre le message dans cette conversation
+
+//         idConv = await Conversation.createConv(message.sender_id, message.participant, message.name);
+//         message.id_conv = idConv;
+//         const mess = new Message(message);
+//         await mess.save(message.sender_id, message.id_conv);
+//       }
+
+//       roomZ = message.id_conv;
+
+//       // io.to(roomZ.toString()).emit('send_message_from_API', message);
+
+//       io.emit('send_message_from_API', message);
+
+//       // io.in(message.id_conv).emit('send_message_from_API', message);
     
       
-      // io.sockets.in(roomZ.toString()).emit('send_message_from_API', message);
+//       // io.sockets.in(roomZ.toString()).emit('send_message_from_API', message);
       
-      console.log('RoomZ=', roomZ);
+//       console.log('RoomZ=', roomZ);
       
-      console.log('Message = ', message);
+//       console.log('Message = ', message);
 
 
 
 
-    } catch (error) {
-      console.log(error);
-    }
+//     } catch (error) {
+//       console.log(error);
+//     }
 
-    // const mess = new Message(message);
+//     // const mess = new Message(message);
 
-    // await mess.save(message.sender_id,message.id_conv);
-    // TEst pour rejoindre room
-    // ws.join(message.id_conv);
-
-
-    // ws.on('room', room =>{
-    //   console.log('R00m:', room);
-    //   io.to(room).emit('send_message_from_server', message)
-    // });
+//     // await mess.save(message.sender_id,message.id_conv);
+//     // TEst pour rejoindre room
+//     // ws.join(message.id_conv);
 
 
+//     // ws.on('room', room =>{
+//     //   console.log('R00m:', room);
+//     //   io.to(room).emit('send_message_from_server', message)
+//     // });
 
-  });
-});
+
+
+//   });
+// });
 
